1e4e1c67de625269430318914883da5b5be8f824
supiccc <supiccc@gmail.com>
2018-08-10 00:47:25 +0800

    增加视图解析

--- src/main/java/com/scau/hyskjf/controller/MemberCenterController.java
+++ src/main/java/com/scau/hyskjf/controller/MemberCenterController.java
@@ -8,14 +8,12 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.servlet.http.HttpSession;
 
