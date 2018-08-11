2b976cafdb1ea75d42019a7c3834c68795b5f648
supiccc <supiccc@gmail.com>
2018-08-12 00:33:07 +0800

    修改demo

--- src/main/java/com/scau/hyskjf/controller/DemoController.java
+++ src/main/java/com/scau/hyskjf/controller/DemoController.java
@@ -1,13 +1,5 @@
 package com.scau.hyskjf.controller;
 
-import com.aliyuncs.DefaultAcsClient;
-import com.aliyuncs.IAcsClient;
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
-import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
-import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.http.MethodType;
-import com.aliyuncs.profile.DefaultProfile;
-import com.aliyuncs.profile.IClientProfile;
 import com.scau.hyskjf.dao.AdminMapper;
 import com.scau.hyskjf.util.SMS.sendSMS;
 import com.scau.hyskjf.util.json.ResponseCode;
