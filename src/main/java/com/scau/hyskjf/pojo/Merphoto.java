c195456b5154e82f8bbd4cdfa369bddcd8552801
supiccc <supiccc@gmail.com>
2018-08-13 22:26:50 +0800

    增加MerPhote表

---
+++ src/main/java/com/scau/hyskjf/pojo/Merphoto.java
@@ -0,0 +1,43 @@
+package com.scau.hyskjf.pojo;
+
+public class Merphoto {
+    private Integer photoid;
+
+    private Integer merid;
+
+    private String photolink;
+
+    private String note;
+
+    public Integer getPhotoid() {
+        return photoid;
+    }
+
+    public void setPhotoid(Integer photoid) {
+        this.photoid = photoid;
+    }
+
+    public Integer getMerid() {
+        return merid;
+    }
+
+    public void setMerid(Integer merid) {
+        this.merid = merid;
+    }
+
+    public String getPhotolink() {
+        return photolink;
+    }
+
+    public void setPhotolink(String photolink) {
+        this.photolink = photolink;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
