1b9ea9fc3db4e1c5bc1e56e04d102bde94b008ba
kevin <532536266@qq.com>
2018-08-16 01:04:10 +0800

    完成商家模糊查找功能

---
+++ src/main/java/com/scau/hyskjf/controller/AdvertiseManagementController.java
@@ -0,0 +1,9 @@
+package com.scau.hyskjf.controller;
+
+/**
+ * @Author: kevin
+ * @Date: 2018/8/15 16:33
+ * @Version 1.0
+ */
+public class AdvertiseManagementController {
+}
---
+++ src/main/java/com/scau/hyskjf/service/impl/AdvertiseManagementServiceImpl.java
@@ -0,0 +1,9 @@
+package com.scau.hyskjf.service.impl;
+
+/**
+ * @Author: kevin
+ * @Date: 2018/8/15 16:42
+ * @Version 1.0
+ */
+public class AdvertiseManagementServiceImpl {
+}
