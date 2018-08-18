2303a0ef66c41d10b119e56459baa7abba84d5b1
supiccc <supiccc@gmail.com>
2018-08-18 09:54:23 +0800

    更新

---
+++ src/main/java/com/scau/hyskjf/pojo/Merchantaccountdetail.java
@@ -0,0 +1,16 @@
+package com.scau.hyskjf.pojo;
+
+/**
+ * Created by supiccc on 2018-08-17 17:00
+ */
+public class Merchantaccountdetail extends Merchantaccount {
+    String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
