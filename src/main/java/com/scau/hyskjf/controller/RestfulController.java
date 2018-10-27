2e2f65b1b3f37b4e4c6654ac8aeb8a4dcc248baf
supiccc <supiccc@gmail.com>
2018-10-28 02:53:25 +0800

    add dockfile

--- src/main/java/com/scau/hyskjf/controller/RestfulController.java
+++ src/main/java/com/scau/hyskjf/controller/RestfulController.java
@@ -4,7 +4,6 @@ import com.scau.hyskjf.service.RedisService;
 import com.scau.hyskjf.util.json.ResponseCode;
 import com.scau.hyskjf.util.json.ResponseJSON;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
